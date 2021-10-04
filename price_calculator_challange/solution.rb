06730eb943acc7974a11bb1df5c741e8bda9505a
DevMasterAryan <gunjackaryan@gmail.com>
2021-10-04 17:33:37 +0530

    solution

--- price_calculator_challange/solution.rb
+++ price_calculator_challange/solution.rb
@@ -1,7 +1,7 @@
 def solution 
   puts "Please enter all the items purchased separated by a comma"
   items = gets.chomp
-  items_list = items.split(",")
+  items_list = items.split(",").map{|e| e.gsub(" ", "")}
   item_quality_hash = items_list.each_with_object(Hash.new(0)) { |item, hash| hash[item]+=1}
   calculate_item_price item_quality_hash 
 end
@@ -9,6 +9,8 @@ end
 
 def calculate_item_price item_quality_hash 
     sum, actual_price = 0 , 0  
+    puts "Item     Quantity    Price"
+    puts "........................."
     item_quality_hash.each do |key, value|
         actual_price = actual_price + value*static_price_hash[key]["unit_price"]
         item_static_detail = static_price_hash[key]
@@ -16,20 +18,26 @@ def calculate_item_price item_quality_hash
         if sale_price
             if sale_price["quantity"] > value
               sum = sum + item_static_detail["unit_price"]
+              puts "#{key}    #{value}    $#{item_static_detail["unit_price"]}"
             else
+              price = 0 
               unit_price_item = value%sale_price["quantity"]
-              sum = sum + unit_price_item * item_static_detail["unit_price"] unless unit_price_item==0
+              price = price + unit_price_item * item_static_detail["unit_price"] unless unit_price_item==0
               quantity_of_sale_price_item = value - unit_price_item 
               if quantity_of_sale_price_item > 0 
-                  sum = sum + (quantity_of_sale_price_item/sale_price["quantity"])*sale_price["price"]
+                  price = price + (quantity_of_sale_price_item/sale_price["quantity"])*sale_price["price"]
               end
+              puts "#{key}      #{value}    $#{price}"
+              sum = sum + price 
             end
         else
           sum = sum + item_static_detail["unit_price"]
+          puts "#{key}      #{value}    $#{item_static_detail["unit_price"]}"
         end
     end
+    puts ""
     puts "Total Price: $#{sum}"
-    puts "You saved: $#{actual_price - sum} today."
+    puts "You saved: $#{(actual_price - sum).round(2)} today."
 end
 
 def static_price_hash 
